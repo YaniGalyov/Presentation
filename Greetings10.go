06c02e6792dc011444900eebd24ad12192293d91
Yani <yani.galiov@gmail.com>
2018-12-07 14:35:17 +0200

    new

--- Greetings10.go
+++ Greetings10.go
@@ -10,6 +10,13 @@ func showGreetings(n []string) {
 
 }
 
+func showAge(y []int) {
+
+	for _, i := range y {
+		fmt.Println("Age:", i)
+	}
+}
+
 func main() {
 
 	n1 := []string{"Yani", "Vlad", "Krasi", "Ivan", "Kris", "Naso", "Dimi", "Geri", "Lili", "Zoya"}
@@ -17,4 +24,8 @@ func main() {
 
 	n2 := []string{"Yani"}
 	showGreetings(n2)
+
+	y := []int{"36", "39", "20", "25", "41", "30", "15", "11", "22", "19"}
+	showAge(y)
+
 }
